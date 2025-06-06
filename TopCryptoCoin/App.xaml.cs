903aa33b3da367b6d244877ca3f56ff5594aa1dd
qwilgsi <orafan100@gmail.com>
2025-06-06 23:09:31 +0300

    Add project files.

---
+++ TopCryptoCoin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TopCryptoCoin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
