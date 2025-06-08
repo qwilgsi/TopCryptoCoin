d842959ccc291b97c310045100b3cc203fda86a3
qwilgsi <orafan100@gmail.com>
2025-06-08 23:39:34 +0300

    Created transition to the main page

--- TopCryptoCoin/ViewModels/MainViewModel.cs
+++ TopCryptoCoin/ViewModels/MainViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace TopCryptoCoin.ViewModels
 {
-    internal class MainViewModel
+    partial class MainPage : Page
     {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
     }
 }
